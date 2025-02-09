2d8d494b783d5c2f50cfc65b7240456343f4ba7e
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2025-02-10 01:50:15 +0500

    Remove debug console logs from EditAnomalyFormDialog component

--- src/components/admin/editAnomaly.tsx
+++ src/components/admin/editAnomaly.tsx
@@ -76,8 +76,6 @@ const EditAnomalyFormDialog: React.FC<AnomalyFormProps> = ({
         // endTime: anomaly.endTime,
         // daysOfWeek: anomaly.daysOfWeek
       };
-      console.log(anomaly.cameras)
-      console.log(anomalyRequest)
 
       const updatedAnomaly = await updateAnomaly(
         organizationId,
